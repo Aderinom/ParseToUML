ce82dbd1f1afce5d3f516ef6275bfce80e249ee0
Aaron Kelbsch <Kelbsch-Aaron@hotmail.com>
2024-03-03 13:33:38 +0100

    fix: deployment paths

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,12 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+
+const repo : string | undefined= process.env["GITHUB_REPOSITORY"]?.split("/")[0]; // owner/repo
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr(), tsconfigPaths()],
+  base: repo
 });
